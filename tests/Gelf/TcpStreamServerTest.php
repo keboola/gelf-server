4a6908e073fb210c29e90f2c7e7b6553f602e5be
Odin <odinuv@gmail.com>
2017-08-29 12:47:47 +0200

    wip: refactor tests to a more declarative style

--- tests/Gelf/TcpStreamServerTest.php
+++ tests/Gelf/TcpStreamServerTest.php
@@ -19,7 +19,7 @@ class TcpStreamServerTest extends \PHPUnit_Framework_TestCase
             12201,
             12201,
             function ($port) use ($process) {
-                $this->assertEquals(12201, $port);
+                self::assertEquals(12201, $port);
                 $process->start();
             },
             function (&$terminated) use ($process) {
@@ -28,30 +28,82 @@ class TcpStreamServerTest extends \PHPUnit_Framework_TestCase
                     $terminated = true;
                 }
             },
-            function ($event) use (&$counter, $testsDir) {
-                $counter++;
-                $file = $testsDir . 'data' . DIRECTORY_SEPARATOR . $counter . ".json";
-                $target = json_decode(file_get_contents($file), true);
-
-                if ($event['short_message'] == 'Exception example') {
-                    $this->assertArrayHasKey('_exception', $event);
-                    $this->assertArrayHasKey('file', $event);
-                    unset($event['_exception']);
-                    unset($target['_exception']);
-                    unset($event['file']);
-                    unset($target['file']);
-                }
-                $this->assertArrayHasKey('timestamp', $event);
-                unset($event['timestamp']);
-                unset($target['timestamp']);
-                $this->assertArrayHasKey('host', $event);
-                unset($target['host']);
-                unset($event['host']);
-                $this->assertEquals($target, $event);
+            function ($event) use (&$events) {
+                $events[] = $event;
             },
-            function () use (&$counter) {
-                $this->assertEquals(6, $counter);
-            }
+            null
         );
+        $timestamps = [];
+        $hosts = [];
+        foreach ($events as $event) {
+            self::assertArrayHasKey('timestamp', $event);
+            self::assertArrayHasKey('host', $event);
+            $timestamps[] = $event['timestamp'];
+            $hosts[] = $event['host'];
+        }
+        self::assertEquals([
+            0 => [
+                'version' => '1.0',
+                'host' => $hosts[0],
+                'short_message' => 'A debug message.',
+                'level' => 7,
+                'timestamp' => $timestamps[0],
+            ],
+            1 => [
+                'version' => '1.0',
+                'host' => $hosts[1],
+                'short_message' => 'An alert message',
+                'level' => 1,
+                'timestamp' => $timestamps[1],
+                '_structure' => [
+                    'data' => [0, 1]
+                ]
+            ],
+            2 => [
+                'version' => '1.0',
+                'host' => $hosts[2],
+                'short_message' => 'Exception example',
+                'level' => 0,
+                'timestamp' => $timestamps[2],
+                'full_message' => "Exception: Test exception (0)\n\n#0 {main}\n",
+                'file' => 'D:\Dropbox\wwwroot\gelf-server\tests\Clients\TcpClient.php',
+                'line' => 18,
+                '_exception' => "Exception: Test exception in D:\Dropbox\wwwroot\gelf-server" .
+                    "\\tests\Clients\TcpClient.php:18\nStack trace:\n#0 {main}",
+            ],
+            3 => [
+                'version' => '1.0',
+                'host' => $hosts[3],
+                'short_message' => 'Structured message',
+                'level' => 1,
+                'timestamp' => $timestamps[3],
+                'full_message' => 'There was a foo in bar',
+                'facility' => 'example-facility',
+                '_foo' => 'bar',
+                '_bar' => 'baz',
+            ],
+            4 => [
+                'version' => '1.0',
+                'host' => $hosts[4],
+                'short_message' => 'A warning message.',
+                'level' => 4,
+                'timestamp' => $timestamps[4],
+                '_structure' => [
+                    'with' => [
+                        'several' => 'nested',
+                        0 => 'levels',
+                    ]
+                ],
+            ],
+            5 => [
+                'version' => '1.0',
+                'host' => $hosts[5],
+                'short_message' =>
+                    file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '..' .
+                        DIRECTORY_SEPARATOR . 'Clients' . DIRECTORY_SEPARATOR . 'bacon.txt'),
+                'level' => 6,
+                'timestamp' => $timestamps[5],
+            ]
+        ], $events);
     }
 }
